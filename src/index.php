3657022db2ba19ff702ee351af9553b8834fa2df
Artpupser <artpupser@proton.me>
2025-10-30 01:26:12 +0300

    0.0.1
    Init

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./public/css/import.css"></link>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Hello world!";
+        ?>
+    </h1>
+    <script href="./public/js/index.js"></script>
+</body>
+</html>
