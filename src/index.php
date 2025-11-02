3653e587034ec0a407eab6ebc1da28151010072e
Artpupser <artpupser@proton.me>
2025-11-02 14:41:08 +0300

    start verstka home page
    Added: index.html, btn.css, flex.css, index.css, tags.css, bg.png, phone.png, place.png

--- src/index.php
+++ src/index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./public/css/import.css"></link>
-</head>
-<body>
-    <h1>
-        <?php 
-        echo "Hello world!";
-        ?>
-    </h1>
-    <script src="./public/js/index.js"></script>
-</body>
-</html>
+<?php 
+include './public/html/index.html'; 
+?>
