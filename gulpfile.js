a4948edce25f0c82b231b42a228645ea42555184
Artpupser <105547651+Artpupser@users.noreply.github.com>
2025-12-15 22:35:27 +0300

    builder project
    Added gulp, stylus, postcss and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+import gulp from 'gulp';
+import stylus from 'gulp-stylus';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import postCssImport from 'postcss-import';
+import postCssNested from 'postcss-nested';
+import postCssPresetEnv from 'postcss-preset-env';
+import mergeRules from 'postcss-merge-rules';
+
+const paths = {
+    css: {
+        src: './src/public/stylus/main.styl',
+        dist: './dist/public/css'
+    },
+    php: {
+        src: ['./src/**/*.php'],
+        dist: ['./dist/'],
+    },
+    images: {
+        src: ['./src/assets/**/*.{png, jpg, jpeg, webp, svg}'],
+        dist: ['./dist/assets/']
+    },
+    fonts: {
+        src: ['./src/assets/fonts/**/*.ttf'],
+        dist: ['./dist/assets/fonts/']
+    },
+    scripts: {
+        src: ['./src/scripts/**/*.{go, py}'],
+        dist: ['./dist/scripts/']
+    }
+};
+
+gulp.task('styles', function () {
+    return gulp.src(paths.css.src) 
+        .pipe(stylus()) 
+        .pipe(postcss([
+            postCssImport(),
+            postCssNested(),
+            postCssPresetEnv({stage: 0}), //0 - эксперементал, 1 - продакшн
+            autoprefixer(),
+            mergeRules(),
+            cssnano({preset: 'default'})])) 
+        .pipe(gulp.dest(paths.css.dist)); 
+});
+
+gulp.task('images', function() {
+    return gulp
+        .src(paths.images.src, { encoding: false }) 
+        .pipe(gulp.dest(paths.images.dist)); 
+});
+
+
+gulp.task('fonts', function(){
+    return gulp
+        .src(paths.fonts.src, { encoding: false })
+        .pipe(gulp.dest(paths.fonts.dist));
+})
+
+gulp.task('scripts', function () {
+    return gulp.src(paths.scripts.src)
+        .pipe(gulp.dest(paths.scripts.dist));
+});
+
+
+gulp.task('php', function () {
+    return gulp.src(paths.php.src)
+        .pipe(gulp.dest(paths.php.dist));
+});
+
+
+
+gulp.task('build', gulp.series('php','images', 'fonts', 'styles', 'fonts', 'scripts'));
+gulp.task('default', gulp.series('build'));
