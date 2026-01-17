86620720d006f4141f8b798a7d9998ec3881864e
AnyaHandina <sashanikrosov@mail.ru>
2026-01-17 10:11:00 +0300

    user is not connected

---
+++ src/views/user.php
@@ -0,0 +1,40 @@
+<div class="container_con">
+    <section class="section_con profile-section">
+        <!-- Профиль пользователя -->
+        <div class="profile-header">
+            <div class="profile-icon">
+                
+            </div>
+            <h2>Профиль пользователя</h2>
+        </div>
+
+        <div class="user-info profile-card">
+            <div class="info-row">
+                <span class="info-label">Имя:</span>
+                <span class="info-value" id="user_name"></span>
+            </div>
+            <div class="info-row">
+                <span class="info-label">Телефон:</span>
+                <span class="info-value"></span>
+            </div>
+            
+        </div>
+    </section>
+    
+    <section class="section_con profile-section">
+        <div class="orders-section">
+            <h2 class="section-title">Ваши заказы</h2>
+            
+            <div class="orders-container">
+                <h3>Ожидают доставки</h3>
+                <div class="orders-list active" id="pending-orders">
+
+                </div>
+                <h3>Завершенные</h3>
+                <div class="orders-list" id="completed-orders">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
