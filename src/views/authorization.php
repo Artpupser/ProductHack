3610ff0169a055322f3323d3bf8405d53faa364b
AnyaHandina <sashanikrosov@mail.ru>
2026-01-23 00:10:58 +0300

    добавила корзину без товаров, пароль в авторизации не видно, оплата открыается от корзины

--- src/views/authorization.php
+++ src/views/authorization.php
@@ -1,35 +1,35 @@
 <div class="container_con">
 	<section class="section_con">
-		<div id="login-form" style="display: block;">
+		<div id="login-form">
 			<h2>Авторизация</h2>
 			<form class="contact-form" method="POST" action="/api/user/login">
 				<label for="login_email">Почта</label>
-				<input type="tel" id="phone" name="email" placeholder="Введите почту" required />
+				<input type="email" id="login_email" name="email" placeholder="Введите почту" required>
 
 				<label for="login_password">Пароль</label>
-				<input type="text" id="password" name="password" placeholder="Введите пароль" required />
+				<input type="password" id="login_password" name="password" placeholder="Введите пароль" required>
 
 				<button type="submit"> Вход</button>
-				<p>Еще не зарегистрировались?</p>
-				<button type="button" onclick="showRegisterForm()">Зарегистрироваться!</button>
+				<p class="switch-p">Еще не зарегистрировались?</p>
+				<button type="button" class="switch-btn" onclick="showRegisterForm()">Зарегистрироваться!</button>
 			</form>
 		</div>
 
-		<div id="register-form" style="display: none;">
+		<div id="register-form" style="display:none">
 			<h2>Регистрация</h2>
 			<form class="contact-form" method="POST" action="/api/user/registration">
 				<label for="registration_email">Почта</label>
-				<input type="email" id="email" name="email" placeholder="Введите почту" required />
+				<input type="email" id="reg_email" name="email" placeholder="Введите почту" required />
 
 				<label for="registration_password">Пароль</label>
-				<input type="password" id="password" name="password" placeholder="Введите Пароль" required />
+				<input type="password" id="reg_password" name="password" placeholder="Введите Пароль" required />
 
 				<label for="registration_repeat_password">Повтор пароля</label>
-				<input type="password" id="repeat_password" name="repeat_password" placeholder="Повтор пароля" required />
+				<input type="password" id="reg_password" name="repeat_password" placeholder="Повтор пароля" required />
 
 				<button type="submit">Зарегистрироваться</button>
-				<p>Уже есть аккаунт?</p>
-				<button type="button" onclick="showLoginForm()">Войти!</button>
+				<p class="switch-p">Уже есть аккаунт?</p>
+				<button type="button" class="switch-btn" onclick="showLoginForm()">Войти!</button>
 			</form>
 		</div>
 	</section>
--- src/views/payment.php
+++ src/views/payment.php
@@ -1,12 +1,23 @@
-<h1 class="payment-title">Выбор способа оплаты</h1>
-<div class="payment-wrapper">
-  <form action="./payment/fake-gateway" method="POST">
-    <label for="payType" class="visually-hidden">Способ оплаты</label>
-    <div class="payment-btns">
-      <button type="submit" name="gateway" value="ukassa" class="payment-btn">СБП (Система быстрых платежей)</button>
-      <button type="submit" name="gateway" value="ukassa" class="payment-btn">Карта «Мир»</button>
-      <button type="submit" name="gateway" value="yandex" class="payment-btn">Яндекс Касса</button>
-      <button type="submit" name="gateway" value="ukassa" class="payment-btn">ЮКасса</button>
-    </div>
-  </form>
-</div>
+<section class="section_pay">
+  <div class="pay-wrapper">
+    <h2 class="pay-title">Выберите способ оплаты</h2>
+
+    <form action="./payment/fake-gateway" method="POST" class="pay-form">
+      <div class="pay-group">
+        <label class="pay-label">Система быстрых платежей</label>
+        <button type="submit" name="gateway" value="sbp" class="pay-btn">СБП</button>
+      </div>
+
+      <div class="pay-group">
+        <label class="pay-label">Карта «Мир»</label>
+        <button type="submit" name="gateway" value="mir" class="pay-btn">Мир</button>
+      </div>
+
+      <div class="pay-group">
+        <label class="pay-label">Перейти на страницу платёжного сервиса</label>
+        <button type="submit" name="gateway" value="yandex" class="pay-btn">Яндекс Касса</button>
+        <button type="submit" name="gateway" value="ukassa" class="pay-btn">ЮКасса</button>
+      </div>
+    </form>
+  </div>
+</section>
