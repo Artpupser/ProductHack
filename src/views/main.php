c622f60626b5d940eba0cf35cd429c70fcc9ca5c
anna <anafilimonova133@gmail.com>
2026-02-03 11:21:33 +0000

    slider and authorization

--- src/views/main.php
+++ src/views/main.php
@@ -1,31 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/api/public/file/?name=css/style.css">
+    <title>Винишко</title>
+</head>
+<body>
+
 <main class="body_main">
-	<div class="background-container">
-		<div class="bg-1">
+    <div class="background-container">
 
-			<h1 class="vinishko">Винишко на все <br> случаи жизни</h1>
-			<button class="butcat" onclick="window.location.href='./catalog'">Перейти в каталог</button>
-		</div>
-		<div class="bg-2">
+        <div class="bg-1">
+            <h1 class="vinishko">Винишко на все <br> случаи жизни</h1>
+            <button class="butcat" onclick="window.location.href='/catalog'">Перейти в каталог</button>
+        </div>
 
-			<div class="slider">
-				<?php for ($i = 1; $i <= 4; $i++)
-					echo "<img class=\"slider-img\" src=\"/api/public/file/?name=imgs/$i.webp\" alt=\"Слайд 1\">" ?>
-					<div class="controls">
-						<img style="opacity: 1; height: 40px;" class="left_controlls"
-							src="/api/public/file/?name=imgs/left_switch_icon.webp" alt="Слева">
-						<img style="opacity: 1; height: 40px;" class="right_controlls"
-							src="/api/public/file/?name=imgs/right_switch_icon.webp" alt="Справа">
-					</div>
-				</div>
-				<a href="/catalog" class="ssilka">
-					Перейти в каталог</a>
-			</div>
+        <div class="bg-2">
+            <div class="slider">
+                <?php 
+                $slideCount = 4; 
+                for ($i = 1; $i <= $slideCount; $i++): ?>
+                    <img class="slider-img" src="/api/public/file/?name=imgs/<?= $i ?>.webp" alt="Слайд <?= $i ?>">
+                <?php endfor; ?>
 
+                <div class="controls">
+                    <img class="left_controlls" src="/api/public/file/?name=imgs/left_switch_icon.webp" alt="Слева">
+                    <img class="right_controlls" src="/api/public/file/?name=imgs/right_switch_icon.webp" alt="Справа">
+                </div>
+            </div>
 
+            <a href="/catalog" class="ssilka">Перейти в каталог</a>
+        </div>
 
-			<div class="acii">
-				<img src="./api/public/file/?name=imgs/sale_banner.webp" alt="">
-			</div>
+        <div class="acii">
+            <img src="/api/public/file/?name=imgs/sale_banner.webp" alt="Баннер">
+        </div>
 
-	</main>
-	<script src="/api/public/file/?name=js/slider.js"></script>
+    </div>
+</main>
+
+<script src="/api/public/file/?name=js/slider.js"></script>
+</body>
+</html>
