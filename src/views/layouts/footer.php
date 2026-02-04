f6e863d4cd8fea6d39383956c28f60e2852683b4
AnyaHandina <sashanikrosov@mail.ru>
2026-02-04 18:50:08 +0300

    я забыла запушить

--- src/views/layouts/footer.php
+++ src/views/layouts/footer.php
@@ -2,9 +2,18 @@
 	<div class="left_foot">
 		<h2 class="title_foot">Винный магазин</h2>
 		<div class="icon_wine_foot">
-			<img src="./public/imgs/logo_icon.webp" alt="Логотип">
+			<img src="/api/public/file/?name=imgs/logo_icon.webp" alt="Логотип">
 		</div>
 		<p class="copy">© 2025 Wine Store Rights Reserved.</p>
+	</div>
+	<div class="place flex-center flex-row">
+		<div class="img-place">
+			<img src="/api/public/file/?name=imgs/place_icon.webp" alt="Местоположение">
+		</div>
+		<div class="flex-column">
+			<p class="my_font">г. Санкт-Петербург,</p>
+			<p class="my_font">ул. Куйбышева 31</p>
+		</div>
 	</div>
 	<div class="right_foot">
 		<nav class="menu_foot">
--- src/views/layouts/header.php
+++ src/views/layouts/header.php
@@ -1,17 +1,9 @@
 <header class="header_main">
 	<div class="logo">
-		<a href="/"><img src="../../public/imgs/logo_icon.webp" alt="Логотип"></a>
+		<a href="/"><img src="/api/public/file/?name=imgs/logo_icon.webp" alt="Логотип"></a>
 	</div>
 	<p class="wineshop"><?php echo $pageTitle ?></p>
-	<div class="place flex-center flex-row">
-		<div class="img-place">
-			<img src="./public/imgs/place_icon.webp" alt="Местоположение">
-		</div>
-		<div class="flex-column">
-			<p class="my_font">г. Санкт-Петербург,</p>
-			<p class="my_font">ул. Куйбышева 31</p>
-		</div>
-	</div>
+	
 	<div class="contacts flex-center flex-row">
 		<?php
 		use ProductHack\models\SessionModel;
@@ -20,10 +12,10 @@
 			<?php
 			$user = $sessionModel->getUser();
 			if ($user->role_id == 2): ?>
-				<a href="./admin"><img src="./public/imgs/admin_icon.webp" alt="Admin panel" class="admin"></a>
+				<a href="./admin"><img src="/api/public/file/?name=imgs/admin_icon.webp" alt="Admin panel" class="admin"></a>
 			<?php endif; ?>
 		<?php endif; ?>
-		<a href="./authorization"><img src="./public/imgs/profile_icon.webp" alt="Авторизация" class="profile"></a>
-		<a href="./cart"><img src="./public/imgs/basket_icon.webp" alt="Корзина" class="cart"></a>
+		<a href="./authorization"><img src="/api/public/file/?name=imgs/profile_icon.webp" alt="Авторизация" class="profile"></a>
+		<a href="./cart"><img src="/api/public/file/?name=imgs/basket_icon.webp" alt="Корзина" class="cart"></a>
 	</div>
 </header>
